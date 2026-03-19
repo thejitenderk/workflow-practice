1fda0e09d1397fcccb673ba05eb40b49715b5377
Jitender Kumar <thejitenderk@gmail.com>
2026-03-19 07:34:11 +0530

    add var ttype

--- environment/dev/variables.tf
+++ environment/dev/variables.tf
@@ -1,10 +1,24 @@
 variable "resource_groups" {
-  
+  description = "Map of resource groups to create"
+  type = map(object({
+    location = string
+  }))
 }
 
 variable "vnets" {
-  
+  description = "Map of VNets with their subnets"
+  type = map(object({
+    resource_group_name = string
+    address_space       = list(string)
+    location            = string
+
+    subnets = list(object({
+      name             = string
+      address_prefixes = list(string)
+    }))
+  }))
 }
+
 
 # variable "linux_vms" {
   
